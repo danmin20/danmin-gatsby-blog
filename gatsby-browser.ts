40be8c545e31ac2fce8c24dd23588dcbe895b92d
lee jeongmin <ljm991108@gmail.com>
2022-11-02 03:14:29 +0900

    feat: normalize -> reset

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,7 +1,7 @@
 // custom typefaces
 import 'typeface-montserrat';
 import 'typeface-merriweather';
-// normalize CSS across browsers
-import './src/styles/normalize.css';
+// reset CSS across browsers
+import './src/styles/reset.css';
 // Highlighting for code blocks
 import 'prismjs/themes/prism.css';
